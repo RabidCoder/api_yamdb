bb39730a0cb36de9b22c002e09dce1f818f83f8b
Alex Roldugin <alexander@roldug.in>
2024-10-29 23:04:14 +0300

    Add Category, Genre, Title models. Add admin register.

---
+++ api_yamdb/reviews/models.py
@@ -0,0 +1,57 @@
+import datetime
+
+from django.contrib.auth import get_user_model
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.utils.text import slugify
+
+User = get_user_model()
+
+
+def current_year():
+    return datetime.date.today().year
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    class Meta:
+        ordering = ['slug', 'name']
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    class Meta:
+        ordering = ['slug', 'name']
+
+    def __str__(self):
+        return self.name
+
+
+class Title(models.Model):
+    name = models.CharField(max_length=256)
+    year = models.IntegerField(
+        validators=[
+            MaxValueValidator(current_year()),
+            MinValueValidator(-20000)
+        ]
+    )
+    description = models.TextField(blank=True, null=True)
+    genre = models.ManyToManyField(
+        Genre, blank=True, null=True
+    )
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, blank=True, null=True
+    )
+
+    class Meta:
+        ordering = ['-year', 'category', 'name']
+
+    def __str__(self):
+        return self.name
