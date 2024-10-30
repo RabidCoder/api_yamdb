5d939c4f072d042fda335d1569fcd08a72cd2afa
Alex Roldugin <alexander@roldug.in>
2024-10-30 10:23:36 +0300

    Model Title change.

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -44,9 +44,7 @@ class Title(models.Model):
         ]
     )
     description = models.TextField(blank=True, null=True)
-    genre = models.ManyToManyField(
-        Genre, blank=True, null=True
-    )
+    genre = models.ManyToManyField(Genre, blank=True)
     category = models.ForeignKey(
         Category, on_delete=models.SET_NULL, blank=True, null=True
     )
