cbc72cf0d4e4e969543a345eea635e8483ce54bb
Alexander Krechetov <Ko4kaNto4kA@yandex.ru>
2024-11-02 18:10:35 +0300

    bug_fix and refactor permissions

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,32 +1,35 @@
 from rest_framework import permissions
 
 
-class AdminOrReadOnly(permissions.BasePermission):
+class CustomPermission(permissions.BasePermission):
     """
-    Permission class checks whether the request user is admin.
-    If not, he/she gets a read only permission.
+    Custom permissions for object access:
+    - Allows safe methods for all users.
+    - Allows all methods for authenticated users.
+    - Only the object author, moderators, and admins can modify or delete the object.
     """
-
+   
     def has_permission(self, request, view):
-        return (
-            request.method in permissions.SAFE_METHODS
-            or request.user.role == 'admin'
-        )
-
-
-class AuthorOrReadOnly(permissions.BasePermission):
-    """Permission class to restrict content modification and deletion."""
-
-    def has_permission(self, request, view):
-        """Allow read permissions for unauthenticated users."""
-        if request.method in permissions.SAFE_METHODS:
+       # Allow safe methods for all users
+       if request.method in permissions.SAFE_METHODS:
             return True
-        return request.user.is_authenticated
+       
+       # Allow other methods only for authenticated users
+       return request.user.is_authenticated
 
     def has_object_permission(self, request, view, obj):
-        """Allow modification and deletion for the author,
-        moderators, and admins."""
-        return (
-            request.method in permissions.SAFE_METHODS
-            or request.user.role in ('moderator', 'admin', 'author')
-        )
+        # Allow safe methods for unauthenticated users
+        if not request.user.is_authenticated:
+            return request.method in permissions.SAFE_METHODS
+        
+        # Allow safe methods for authenticated users
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        
+        # Check if the object has an 'author' attribute
+        if hasattr(obj, 'author'):
+            # Allow modifications for the author, moderators, and admins
+            return obj.author == request.user or request.user.role in ('moderator', 'admin')
+        
+        # Allow modifications for moderators and admins if no 'author' attribute
+        return request.user.role in ('moderator', 'admin')
